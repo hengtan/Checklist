6330e63c1665a28bc526c11ff3c012fa2f573813
hengjt <hengtan@live.com>
2016-07-12 17:33:37 -0300

    Sec Commit

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,53 @@
+class ItemsController < ApplicationController
+
+  before_action :find_item, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @items = Item.all.order("created_at DESC")
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+
+  end
+
+
+  private
+
+    def item_params
+      params.require(:item).permit(:title, :description)
+    end
+
+    def find_item
+      @item = Item.find(params[:id])
+    end
+end
